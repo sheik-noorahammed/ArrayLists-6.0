d3afc54a5f02e3904c6e792f02750b5310205c0d
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-28 18:46:02 +0530

    save

--- Create.java
+++ Create.java
@@ -1,7 +1,17 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Load Values in ArrayList
-  // Iterate it by usig Iterator and While Loops
-}
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Create {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Date");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
+    }
 }
